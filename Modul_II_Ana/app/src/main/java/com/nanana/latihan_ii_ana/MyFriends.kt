2e53012b69f1000bec1e8a09d8c939cc86fc8f09
ana bellatus <anabellatus@gmail.com>
2020-05-11 02:06:05 +0700

    (+) Recycleview

---
+++ Modul_II_Ana/app/src/main/java/com/nanana/latihan_ii_ana/MyFriends.kt
@@ -0,0 +1,10 @@
+package com.nanana.latihan_ii_ana
+
+data class MyFriends(
+    val nama: String,
+    val kelamin: String,
+    val email: String,
+    val telp: String,
+    val alamat: String
+) {
+}
--- Modul_II_Ana/app/src/main/java/com/nanana/latihan_ii_ana/MyFriendsFragment.kt
+++ Modul_II_Ana/app/src/main/java/com/nanana/latihan_ii_ana/MyFriendsFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_my_friends.*
 
 class MyFriendsFragment : Fragment() {
+
+    lateinit var listTeman: ArrayList<MyFriends>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,23 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun tampilTeman(){
+        rv_listMyFriends.layoutManager = LinearLayoutManager(activity)
+        rv_listMyFriends.adapter = MyFriendAdapter(activity!!, listTeman)
+    }
+
+    private fun initView(){
+        simulasiDataTeman()
+        tampilTeman()
+    }
+
+    private fun simulasiDataTeman(){
+        listTeman = ArrayList()
+        listTeman.add(MyFriends("Ana", "Perempuan", "anabellatus@gmail.com", "0895342613748", "Jalan di rumah aja"))
+        listTeman.add(MyFriends("Bella", "Perempuan", "bellatusana@gmail.com", "0895342613748", "Jalan sendiri"))
     }
 
 }
