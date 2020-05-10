56c1c1394b2c88749a54987492026034d9731e5a
ana bellatus <anabellatus@gmail.com>
2020-05-10 15:40:02 +0700

    (+) project

---
+++ Modul_II_Ana/app/src/main/java/com/nanana/latihan_ii_ana/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nanana.latihan_ii_ana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
